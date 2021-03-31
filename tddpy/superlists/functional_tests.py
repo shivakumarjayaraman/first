d666598223ed31ddb84040f4987fb1c49bedc2a7
shivajayaraman@gmail.com <shivajayaraman@gmail.com>
2021-03-31 16:35:39 +0530

    Adding files required for templatizing

--- tddpy/superlists/functional_tests.py
+++ tddpy/superlists/functional_tests.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase) :
@@ -17,11 +19,24 @@ class NewVisitorTest(unittest.TestCase) :
         self.browser.get('http://localhost:8000')
 
         self.assertIn('To-Do', self.browser.title)
-        ##self.fail('Finish the test')
+
+        header_text = self.browser.find_element_by_tag_name("h1").text
+        self.assertIn("To-Do", header_text)
 
         ## She is invited to enter a to-do item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         ## She types 'buy peacock flowers' into a text box
+        inputbox.send_keys('Buy peacock feathers')
+
+        inputbox.send_keys(Keys.ENTER) 
+        time.sleep(1) 
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')  1
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
 
         ## Page should list the item
 
--- tddpy/superlists/lists/tests.py
+++ tddpy/superlists/lists/tests.py
@@ -2,18 +2,30 @@ from django.test import TestCase
 from django.urls import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase) :
 
     def test_root_url_resolves_to_home_page(self) :
         found = resolve('/')
+        print ("Inside 1st test")
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self) :
         request = HttpRequest()
         response = home_page(request)
+        print ("Inside 2nd test")
+
+        html = response.content.decode('utf8')
+        expected_html = render_to_string('home.html')
+        self.assertEqual(html, expected_html)
+
+    def test_home_page_returns_correct_html2(self) :
+        response = self.client.get('/')
 
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+
+        self.assertTemplateUsed(response, 'home.html')
